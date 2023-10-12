608ea2b950f8b8e965a0c05f37014a57862c039d
RIENITZ-PC\Rienitz <yosr481@gmail.com>
2023-10-12 14:18:39 +0300

    Saving system

---
+++ Assets/Scripts/Saving/SerializibleVector3.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class SerializibleVector3
+{
+    private float x, y, z;
+
+    public SerializibleVector3(Vector3 vector)
+    {
+        x = vector.x;
+        y = vector.y;
+        z = vector.z;
+    }
+
+    public Vector3 ToVector()
+    {
+        return new Vector3(x, y, z);
+    }
+}
