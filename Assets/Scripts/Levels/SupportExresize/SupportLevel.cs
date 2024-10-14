8db4ced4464fcd6a89e900e0cb26b1b35489549d
amit800IL <108745954+amit800IL@users.noreply.github.com>
2024-10-14 13:09:53 +0300

    support levels progress

--- Assets/Scripts/Levels/SupportExresize/SupportLevel.cs
+++ Assets/Scripts/Levels/SupportExresize/SupportLevel.cs
@@ -5,13 +5,34 @@ using UnityEngine.UI;
 
 public class SupportLevel : MonoBehaviour
 {
+    [SerializeField] private SupportLevelsContainer supportLevelsContainer;
+    [SerializeField] private SupporteExerciseSO supporteExerciseSO;
+    [SerializeField] private TextMeshProUGUI levelText;
+    [SerializeField] private TextMeshProUGUI finishText;
     [SerializeField] private Button writeButton;
+    [SerializeField] private Button continueButton;
+    [SerializeField] private Button backButton;
     private const int maxCharchters = 1000;
 
 
     private void Start()
     {
+        levelText.text = supporteExerciseSO.ExerciseText;
+
+        finishText.text = supporteExerciseSO.FinishText;
+
+        finishText.gameObject.SetActive(false);
+
         writeButton.onClick.AddListener(WriteText);
+        continueButton.onClick.AddListener(supportLevelsContainer.RegisterLevelEnd);
+        backButton.onClick.AddListener(LevelFinish);
+    }
+
+    private void OnDisable()
+    {
+        writeButton.onClick.RemoveListener(WriteText);
+        continueButton.onClick.RemoveListener(supportLevelsContainer.RegisterLevelEnd);
+        backButton.onClick.RemoveListener(LevelFinish);
     }
 
     public void WriteText()
@@ -19,5 +40,8 @@ public class SupportLevel : MonoBehaviour
         TouchScreenKeyboard.Open(" ", TouchScreenKeyboardType.Default);
     }
 
-
+    public void LevelFinish()
+    {
+        finishText.gameObject.SetActive(true);
+    }
 }
